7ad7dddb2a550cb0d32e829576156c4130445764
Jordan Jordanov <d_yugioh@abv.bg>
2017-02-10 14:36:05 +0200

    Hand-made Web Server
    Lab: Handmade HTTP Server
    Problems for exercises and homework for the “C# Web Basics” course @ SoftUni.

---
+++ Hand-made Web Server/SharpStore/SharpStore.cs
@@ -0,0 +1,65 @@
+﻿using SimpleHttpServer.Models;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SharpStore
+{
+    class SharpStore
+    {
+        static void Main(string[] args)
+        {
+            var routes = new List<Route>()
+            {
+                new Route()
+                {
+                    Name = "Home Directory",
+                    Method = SimpleHttpServer.Enums.RequestMethod.GET,
+                    UrlRegex = "^/home$",
+                    Callable = (request) =>
+                    {
+                        return new HttpResponse()
+                        {
+                            StatusCode = SimpleHttpServer.Enums.ResponseStatusCode.Ok,
+                            ContentAsUTF8 = File.ReadAllText("../../content/home.html")
+                        };
+                    }
+                },
+                new Route()
+                {
+                    Name = "Carousel CSS",
+                    Method = SimpleHttpServer.Enums.RequestMethod.GET,
+                    UrlRegex = "^/content/css/carousel.css$",
+                    Callable = (request) =>
+                    {
+                        var response = new HttpResponse()
+                        {
+                            StatusCode = SimpleHttpServer.Enums.ResponseStatusCode.Ok,
+                            ContentAsUTF8 = File.ReadAllText("../../content/css/carousel.css")
+                        };
+                        response.Header.ContentType = "text/css";
+                        return response;
+                    }
+                },
+                new Route()
+                {
+                    Name = "Bootstrap JS",
+                    Method = SimpleHttpServer.Enums.RequestMethod.GET,
+                    UrlRegex = "^/bootstrap/js/bootstrap.min.js$",
+                    Callable = (request) =>
+                    {
+                        var response = new HttpResponse()
+                        {
+                            StatusCode = SimpleHttpServer.Enums.ResponseStatusCode.Ok,
+                            ContentAsUTF8 = File.ReadAllText("../../content/bootstrap/js/bootstrap.min.js")
+                        };
+                        response.Header.ContentType = "application/x-javascript";
+                        return response;
+                    }
+                },
+                //TODO Add route for bootstrap.min.css
+            };
+
+            //TODO: Create new HTTP Server and run it
+        }
+    }
+}
---
+++ Hand-made Web Server/SimpleHttpServer/Models/HttpRequest.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleHttpServer.Models
+{
+    using Enums;
+
+    public class HttpRequest
+    {
+        public RequestMethod Method { get; set; }
+        public string Url { get; set; }
+        //public string Path { get; set; } // either the Url, or the first regex group
+        public string Content { get; set; }
+
+        public Header Header { get; set; }
+
+        public HttpRequest()
+        {
+            this.Header = new Header(HeaderType.HttpRequest);
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} HTTP/1.0\r\n{2}{3}",
+                this.Method,
+                this.Url,
+                this.Header,
+                this.Content);
+        }
+    }
+}
