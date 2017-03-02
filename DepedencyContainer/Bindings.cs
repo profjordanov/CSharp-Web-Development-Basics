de92787d3edfa746b005421dcfee3dd241fc6a47
Jordan Jordanov <d_yugioh@abv.bg>
2017-03-02 21:00:16 +0200

    data

---
+++ DepedencyContainer/Bindings.cs
@@ -0,0 +1,14 @@
+﻿using Ninject.Modules;
+using PizzaForum.Data;
+using PizzaForum.Data.Contracts;
+
+namespace PizzaForum.DepedencyContainer
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<IUnitOfWork>().To<UnitOfWork>();
+        }
+    }
+}
---
+++ Models/Reply.cs
@@ -0,0 +1,25 @@
+﻿namespace PizzaForum.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Reply
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Topic")]
+        public int TopicId { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime? PublishDate { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public virtual User Author { get; set; }
+
+        public virtual Topic Topic { get; set; }
+        
+
+    }
+}
