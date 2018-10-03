0ee6bc9cdb0f57f155640cbe2e76932a18359314
Jordan Jordanov <d_yugioh@abv.bg>
2018-10-03 09:33:22 +0300

    Asynchronous Processing demos

---
+++ AsynchronousProcessing/RaceConditionDemo/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RaceConditionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			object lockObj = new object(); //Solution
+			var threads = new List<Thread>();
+	        int num = 0;
+			try
+			{
+		        for(int i = 0; i < 8; i++)
+		        {
+			        var thread = new Thread(() =>
+			        {
+				        for (int j = 0; j < 10000; j++)
+				        {
+					        if (j == 100)
+					        {
+						        throw new Exception();
+					        }
+					        //Interlocked.Increment(ref num);
+							lock(lockObj)
+							{
+								num++;
+							}
+						}
+
+						Console.WriteLine($"Thread #{Thread.CurrentThread.ManagedThreadId} finished.");
+			        });
+					threads.Add(thread);
+					thread.Start();
+		        }
+			}
+	        catch (Exception e)
+	        {
+		        Console.WriteLine("!!!!!!!" + e);
+	        }
+
+	        foreach (var thread in threads)
+	        {
+		        thread.Join();
+	        }
+	        Console.WriteLine(num);
+		}
+
+	    private void ExampleTwo() //PROBLEM
+	    {
+		    var threads = new List<Thread>();
+		    int num = 0;
+		    try
+		    {
+			    for(int i = 0; i < 2; i++)
+			    {
+				    var thread = new Thread(() =>
+				    {
+					    for(int j = 0; j < 10000; j++)
+					    {
+						    num++;
+						    /*
+						     * get num value to tmp
+						     * add 1 to tmp
+						     * set tmp to num
+						     */
+					    }
+
+					    Console.WriteLine($"Thread #{Thread.CurrentThread.ManagedThreadId} finished.");
+				    });
+				    threads.Add(thread);
+				    thread.Start();
+			    }
+		    }
+		    catch(Exception e)
+		    {
+			    Console.WriteLine(e);
+		    }
+
+		    foreach(var thread in threads)
+		    {
+			    thread.Join();
+		    }
+		    Console.WriteLine(num);
+		}
+
+		private void ExampleOne()
+	    {
+		    List<int> numbers = Enumerable.Range(0 , 10000).ToList();
+		    for(int i = 0; i < 4; i++)
+		    {
+			    new Thread(() =>
+			    {
+				    while(numbers.Count > 0)
+				    {
+					    numbers.RemoveAt(numbers.Count - 1);
+				    }
+			    }).Start();
+		    }
+		}
+    }
+}
---
+++ AsynchronousProcessing/TaskParallelLibraryDemos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskParallelLibraryDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        NumberOfPrimesInIntervalAsync(2, 10000)
+		        .ContinueWith((resultTask) => Console.WriteLine(resultTask.Result));
+        }
+
+	    public static Task<int> NumberOfPrimesInIntervalAsync(int min, int max)
+			=> Task.Run(() => NumberOfPrimesInInterval(min , max));
+
+
+		public static int NumberOfPrimesInInterval(int min, int max)
+	    {
+		    var count = 0;
+		    for (var i = min; i < max; i++)
+		    {
+			    var isPrime = true;
+			    for (var j = 2; j < i; j++)
+			    {
+				    if (i % j != 0)
+				    {
+					    continue;
+					}
+
+				    isPrime = false;
+				    break;
+			    }
+
+			    if (isPrime)
+			    {
+				    count++;
+			    }
+		    }
+
+		    return count;
+	    }
+
+    }
+}
